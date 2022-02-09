aec14739a3701a148dd4328d87a85fa9dabdd8fa
Fran <francinele.coliveira@gmail.com>
2022-02-09 16:48:24 -0300

    finaliza requisito 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header>
-          <Route path="/" component={ Home } />
-        </header>
-      </div>
+      <BrowserRouter>
+        <Route exact path="/" component={ Home } />
+      </BrowserRouter>
     );
   }
 }
