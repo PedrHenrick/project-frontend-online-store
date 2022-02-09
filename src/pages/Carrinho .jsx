da5bcae11da70a1add45ce1a9ea2b0783cecb5ed
Carol Silva <carolyla@hotmail.com>
2022-02-09 20:06:31 -0300

    Grupo 10 - Requisito 03

---
+++ src/pages/Carrinho .jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Carrinho extends React.Component {
+  render() {
+    return (
+      <h3 data-testid="shopping-cart-empty-message"> Seu carrinho está vazio.</h3>
+    );
+  }
+}
+export default Carrinho;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,37 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { BsCartPlus } from 'react-icons/bs';
 
 export default class Home extends React.Component {
-  render() {
-    return (
-      <div>
-        <label htmlFor="search">
-          <input
-            type="text"
-            id="search"
-          />
-        </label>
-        <h3
-          data-testid="home-initial-message"
-        >
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </h3>
-      </div>
-    );
+  state = {
+    redirect: false,
+  };
+
+handleClick = async () => {
+  this.setState({ redirect: true });
+};
+
+render() {
+  const { redirect } = this.state;
+  if (redirect) {
+    return <Redirect to="/carrinho" />;
   }
+  return (
+    <div>
+      <label htmlFor="search">
+        <input type="text" id="search" />
+      </label>
+      <h3 data-testid="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </h3>
+      <button
+        data-testid="shopping-cart-button"
+        type="submit"
+        onClick={ this.handleClick }
+      >
+        <BsCartPlus size={ 30 } />
+      </button>
+    </div>
+  );
+}
 }
